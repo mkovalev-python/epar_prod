74b1636907fc7dee627d68b7026151d0c716936d
mkovalev-python <78082300+mkovalev-python@users.noreply.github.com>
2021-06-02 14:08:47 +0300

    Update tasks.py

--- PManager/viewsExt/tasks.py
+++ PManager/viewsExt/tasks.py
@@ -431,7 +431,7 @@ def __save_doc_from_task_list(task_list):
 
     doc = DocxTemplate("./tracker/media/company2.docx")
     context = {'company_name': task_list}
-    doc.render(context)
+    doc.render(context, encoding="utf8")
     src = "./tracker/media/Report_" + datetime.datetime.now().strftime(
         "%d_%m_%Y_%H:%M") + ".docx"
 
