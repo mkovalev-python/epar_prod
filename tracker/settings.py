d5f2f46c0f8091732de813215e657b8f7282406b
mkovalev-python <78082300+mkovalev-python@users.noreply.github.com>
2021-06-02 13:04:05 +0300

    Update settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -70,7 +70,7 @@ MEDIA_ROOT = PROJECT_ROOT + 'tracker/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/protected/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
