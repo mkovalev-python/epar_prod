f897ca586fbde34ec3f0f99955a536a93f2a3fff
mkovalev <mkovalevhse@yandex.ru>
2021-06-04 11:06:10 +0300

    222222

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,7 @@ python manage.py schemamigration PManager --init
 #python manage.py schemamigration PManager --auto
 python manage.py migrate PManager
 #python manage.py migrate sessions
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+echo "from django.contrib.sites.models import Site; site = Site.objects.create(domain='example.com', name='example.com'); site.save()" | python manage.py shell
 
 gunicorn tracker.wsgi:application -b 0.0.0.0:8000 --reload
