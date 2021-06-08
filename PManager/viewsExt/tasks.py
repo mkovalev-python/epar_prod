0519962fe7910849524fe8ae1efadfc62d391ed9
mkovalev-python <mkovalevhse@yandex.ru>
2021-06-08 14:32:21 +0300

    32

--- PManager/viewsExt/tasks.py
+++ PManager/viewsExt/tasks.py
@@ -424,17 +424,51 @@ def __save_xls_from_task_list(task_list, project, user):
     return file.src
 
 def __save_doc_from_task_list(task_list):
-    from docxtpl import DocxTemplate
-    from jinja2 import Environment, FileSystemLoader
+    from docx import Document
+    from docx.shared import Inches
 
-    doc = DocxTemplate(r'./tracker/media/company2.docx')
-    context = {'company_name':task_list}
-    env = Environment(loader=FileSystemLoader('./tracker/media',encoding='latin-1'))
-    doc.render(context, env)
-    src = "./tracker/media/Report_" + datetime.datetime.now().strftime(
-        "%d_%m_%Y_%H_%M") + ".docx"
+    document = Document()
 
-    doc.save(src)
+    document.add_heading('Document Title', 0)
+
+    p = document.add_paragraph('A plain paragraph having some ')
+    p.add_run('bold').bold = True
+    p.add_run(' and some ')
+    p.add_run('italic.').italic = True
+
+    document.add_heading('Heading, level 1', level=1)
+    document.add_paragraph('Intense quote', style='Intense Quote')
+
+    document.add_paragraph(
+        'first item in unordered list', style='List Bullet'
+    )
+    document.add_paragraph(
+        'first item in ordered list', style='List Number'
+    )
+
+
+    records = (
+        (3, '101', 'Spam'),
+        (7, '422', 'Eggs'),
+        (4, '631', 'Spam, spam, eggs, and spam')
+    )
+
+    table = document.add_table(rows=1, cols=3)
+    hdr_cells = table.rows[0].cells
+    hdr_cells[0].text = 'Qty'
+    hdr_cells[1].text = 'Id'
+    hdr_cells[2].text = 'Desc'
+    for qty, id, desc in records:
+        row_cells = table.add_row().cells
+        row_cells[0].text = str(qty)
+        row_cells[1].text = id
+        row_cells[2].text = desc
+
+    document.add_page_break()
+
+
+    src = "./tracker/media/Report_" + datetime.datetime.now().strftime("%d_%m_%Y_%H_%M") + ".docx"
+    document.save(src)
 
     return "/protected/media/Report_" + datetime.datetime.now().strftime("%d_%m_%Y_%H_%M") + ".docx"
 
