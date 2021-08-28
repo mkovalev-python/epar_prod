0ace0e186e45f9991fe9010a99ce33154102889a
mkovalev-python <78082300+mkovalev-python@users.noreply.github.com>
2021-08-28 20:18:08 +0300

    Update urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.shortcuts import HttpResponse
 from PManager.views import MainPage, Brains, add_timer, PaymentReport, MilestonesReport, DeveloperWorkLoadReport, \
-    ApiPostTask,ApiPostFile
+    ApiPostTask,ApiPostFile,ApiPostText
 from PManager.viewsExt.assets import protected_file, stat_excel
 from PManager.viewsExt.file_view import docxView
 from PManager.viewsExt.files import fileSave, ajaxFilesResponder, AjaxFileUploader, DeleteUploadedFile, AjaxAddFile, \
@@ -162,7 +162,8 @@ urlpatterns = patterns(
     url(r'^rules/', include('company_rules.urls')),
     url(r'^support_form/', include('support_page.urls', namespace='support_page')),
     url(r'^api-past-file/', ApiPostFile),
-    url(r'^api-create-task/', ApiPostTask)
+    url(r'^api-create-task/', ApiPostTask),
+    url(r'^api-create-text/', ApiPostText),
 )
 
 urlpatterns += staticfiles_urlpatterns()
