fc3a6f5f35ffbd28237dbbb3a8651c9ea904d441
mkovalev-python <78082300+mkovalev-python@users.noreply.github.com>
2021-09-01 14:52:02 +0300

    Update report.py

--- report.py
+++ report.py
@@ -11,7 +11,7 @@ def report(tree_tasks):
     doc = DocxTemplate("./tracker/media/template.docx")
     context = {'company_name': "World company"}
     context = {'company_name': escape("World company")}
-    doc.render(context.encode('utf-8')
+    doc.render(context.encode('utf-8'))
 
     src = "./tracker/media/Report_" + datetime.datetime.now().strftime(
         "%d_%m_%Y_%H_%M") + ".docx"
