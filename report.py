90c51a9ac4c496c75f549b62ac48725f01cb7f54
mkovalev-python <78082300+mkovalev-python@users.noreply.github.com>
2021-09-03 10:21:56 +0300

    Update report.py

--- report.py
+++ report.py
@@ -4,16 +4,34 @@ from docxtpl import DocxTemplate
 
 def report(tree_tasks):
     from docxtpl import DocxTemplate
-    from cgi import escape
-    import sys
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
+    import jinja2
 
-    doc = DocxTemplate("./tracker/media/template (1).docx")
-    context = {'company_name': "World company"}
-    doc.render(context)
+    jinja_env = jinja2.Environment()
+
+
+    # to create new filters, first create functions that accept the value to filter
+    # as first argument, and filter parameters as next arguments
+    def my_filterA(value, my_string_arg):
+        return_value = value + ' ' + my_string_arg
+        return return_value
+
+
+    def my_filterB(value, my_float_arg):
+        return_value = value + my_float_arg
+        return return_value
+
+
+    # Then, declare them to jinja like this :
+    jinja_env.filters['my_filterA'] = my_filterA
+    jinja_env.filters['my_filterB'] = my_filterB
+
+
+    context = {'company_name': ' Hello', 'base_value_float': 1.5}
+
+    tpl = DocxTemplate("./tracker/media/template (1).docx")
+    tpl.render(context, jinja_env)
 
     src = "./tracker/media/Report_" + datetime.datetime.now().strftime(
         "%d_%m_%Y_%H_%M") + ".docx"
-    doc.save(src)
+    tpl.save(src)
     return "/protected/media/Report_" + datetime.datetime.now().strftime("%d_%m_%Y_%H_%M") + ".docx"
