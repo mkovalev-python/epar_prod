8e21932d60dc3f1cf9468f6ec15c00c8b94778b5
mkovalev-python <78082300+mkovalev-python@users.noreply.github.com>
2021-09-03 21:38:57 +0300

    Update report.py

--- report.py
+++ report.py
@@ -2,18 +2,19 @@ import datetime
 from docxtpl import DocxTemplate
 
 def report(tree_tasks):
-    from docxtpl import DocxTemplate
-    import jinja2
-
-    jinja_env = jinja2.Environment()
-
-    context = {'company_name': 'Hello'}
-    print(context)
-    tpl = DocxTemplate("./tracker/media/template (1).docx")
-    print(tpl.docx.element.xml)
-    tpl.render(context, jinja_env)
-
-    src = "./tracker/media/Report_" + datetime.datetime.now().strftime(
-        "%d_%m_%Y_%H_%M") + ".docx"
-    tpl.save(src)
-    return "/protected/media/Report_" + datetime.datetime.now().strftime("%d_%m_%Y_%H_%M") + ".docx"
+    from django.core.files.base import ContentFile, File
+    from django.core.files.storage import default_storage
+    list_task = []
+    for _ in task_list:
+        list_task.append(
+            {'name': _.name, 'text': _.text, 'text_eo': _.text_eo, 'prefix':_.prefix,
+             'text_ro': _.text_ro})
+    response = requests.get(
+        "http://94.26.245.131:4000/report/",
+        data={'data': json.dumps(list_task)},
+        verify=False)
+    if response.status_code == 200:
+        path = default_storage.save('Report1.docx', ContentFile(response.content))
+        return "/protected/media/"+path.encode('utf-8')
+    else:
+        return "/"
