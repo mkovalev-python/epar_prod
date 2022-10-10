93dc869c4397747a00842e19a7e0506e21ab707f
maxim <makccom0@gmail.com>
2022-10-10 13:18:51 +0300

    fix

--- PManager/models/users.py
+++ PManager/models/users.py
@@ -332,7 +332,7 @@ class PM_User(models.Model):
     def setRole(self, roleCode, project):
         if self.user and project and roleCode:
             try:
-                clientRole = PM_Role.objects.filter(code=roleCode).first()
+                clientRole = PM_Role.objects.filter(code=roleCode)[0]
             except PM_Role.DoesNotExist:
                 return False
 
